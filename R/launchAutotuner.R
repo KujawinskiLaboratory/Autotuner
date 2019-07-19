e3451d4a6fce0a4643c5dcdf73c68c73442294cd
Craig McLean <crmclean@mit.edu>
2019-07-19 12:52:39 -0400

    Removed GUI from package

--- R/launchAutotuner.R
+++
@@ -1,8 +0,0 @@
-#' @title launchAutotuner
-#'
-#' @description This function deploys the Autotuner shiny app.
-#'
-#' @export
-launchAutotuner <- function() {
-    shiny::runApp(appDir = system.file("application/", package = "Autotuner"))
-}
